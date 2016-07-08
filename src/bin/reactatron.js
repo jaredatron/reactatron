f7f676e60be63ecf64adf233020ac20e3d81ddba
Jared Grippe <jared@deadlyicon.com>
2016-07-08 12:28:48 -0700

    WIP

--- src/bin/reactatron.js
+++ src/bin/reactatron.js
@@ -1,39 +1,41 @@
 #!/usr/bin/env node
 
-import Reactatron from '..'
 import path from 'path'
+require.main.paths.push(path.join(process.cwd(), 'node_modules'))
+
+import Reactatron from '..'
+import Server from '../server'
 import childProcess from 'child_process'
 
-console.log('Reactatron', Reactatron)
-
-
-// babel -d dist/ src/
-// console.log('???', process.argv)
-console.log('???', require.main)
-console.log('CWD', process.cwd())
+const exec = (cmd, args) => {
+  // console.log('EXEC', cmd, args)
+  childProcess.execFile(cmd, args, (error, stdout, stderr) => {
+    if (error) throw error
+    console.log(stdout)
+  })
+}
 
 // const PATH_TO_BABEL = path.join(__filename, '../../../node_modules/.bin/babel')
 const PATH_TO_BABEL = path.join(process.cwd(), 'node_modules/.bin/babel')
+const BABEL_ARGS = [
+  '--no-babelrc',
+  '--copy-files',
+  '--presets', 'babel-preset-react,babel-preset-es2015',
+  '--only', 'src/server,src/client',
+  '-d', 'dist/', 'src/'
+]
 
-
-const exec = (cmd, args) => {
-  console.log('EXEC', cmd, args)
-  childProcess.execFile(cmd, args, (error, stdout, stderr) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  });
+const BABEL_WATCH_ARGS = ['--watch'].concat(BABEL_ARGS)
+if (process.argv[2] === 'compile'){
+  exec(PATH_TO_BABEL, BABEL_ARGS)
 }
 
-if (process.argv[2] === 'compile'){
+if (process.argv[2] === 'watch'){
+  exec(PATH_TO_BABEL, BABEL_WATCH_ARGS)
+}
 
-  exec('echo', ['fuck']);
-
-  exec(PATH_TO_BABEL, [
-    '--no-babelrc',
-    '--presets', 'babel-preset-react', 'babel-preset-es2015',
-    '-d', 'dist/', 'src/'
-  ])
-
+if (process.argv[2] === 'server'){
+  Server.start(()=>{
+    console.log('http://localhost:'+Server.get('port'))
+  })
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,26 @@
 // Reactatron.Server
 
+import path from 'path'
 import express from 'express'
 import bodyParser from'body-parser'
 
 const Server = express()
+const publicDir = path.join(process.cwd(), 'dist/client')
+
+console.log('publicDir', publicDir)
+
+Server.set('port', process.env.PORT || '3000')
 
 Server.use(bodyParser.json())
-// Server.use(express.static(publicDir))
+Server.use(express.static(publicDir))
 
-// Server.get('/*', (request, response) => {
-//   response.sendFile(publicDir + '/index.html');
-// });
+Server.get('/*', (request, response) => {
+  response.sendFile(publicDir + '/index.html');
+});
 
 Server.start = (callback) => {
   Server.listen(Server.get('port'), () => {
-    callback(Server);
+    if (callback) callback(Server);
   });
 };
 

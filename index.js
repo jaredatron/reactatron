0448ec333665749e59336f92ed2e4411a11fe23b
Jared Grippe <jared@deadlyicon.com>
2016-07-08 09:52:05 -0700

    WIP

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ require('babel-core');
 require('babel-register')({
   presets: ['babel-preset-react', 'babel-preset-es2015-node'],
 });
-require("babel-register")
 
 module.exports = require('./lib')
